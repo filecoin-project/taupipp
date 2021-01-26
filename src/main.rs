25b07fe4a4158cee5bcd3098a286dcd62d4115db
nikkolasg <nikkolasg@gmail.com>
2021-01-26 11:12:06 +0000

    read hash bytes first

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 //use filecoin::accumulator::Accumulator as facc;
 use std::fs::OpenOptions;
-use std::io::{self, BufReader, BufWriter, Write};
+use std::io::{self, BufReader, Read};
 use zcash::{Accumulator as zacc, CheckForCorrectness, UseCompression};
 
 fn read_zcash_powers(fname: &str) {
@@ -8,7 +8,16 @@ fn read_zcash_powers(fname: &str) {
         .read(true)
         .open(fname)
         .expect(&format!("unable open {} in this directory", fname));
-    let accumulator = zacc::deserialize(&mut reader, UseCompression::No, CheckForCorrectness::Yes)
+    {
+        // We don't need to do anything with it, but it's important for
+        // the hash chain.
+        let mut tmp = [0; 64];
+        reader
+            .read(&mut tmp)
+            .expect("unable to read BLAKE2b hash of previous contribution");
+    }
+
+    let accumulator = zacc::deserialize(&mut reader, UseCompression::Yes, CheckForCorrectness::Yes)
         .expect("unable to read uncompressed accumulator");
 }
 fn main() {
