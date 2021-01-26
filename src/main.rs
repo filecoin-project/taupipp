be00e5b5c2eac1652d0a7c13e49e0ac2e727b052
nikkolasg <nikkolasg@gmail.com>
2021-01-26 15:05:13 +0000

    correct length for filecoin and zcash

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,31 @@ use std::fmt;
 use std::fs::OpenOptions;
 use std::io::{self, BufReader, Error, Read};
 use zcash::{Accumulator as zacc, CheckForCorrectness, UseCompression};
-const G1_COMPRESSED_BYTE_SIZE: usize = 48;
-const G2_COMPRESSED_BYTE_SIZE: usize = 96;
-const TAU_POWERS_LENGTH: usize = 1 << 21;
+/// TauParams contains the size of the vector of the tau^i vector in g1 and g2
 /// More tau powers are needed in G1 because the Groth16 H query
 /// includes terms of the form tau^i * (tau^m - 1) = tau^(i+m) - tau^i
 /// where the largest i = m - 2, requiring the computation of tau^(2m - 2)
 /// and thus giving us a vector length of 2^22 - 1.
-const TAU_POWERS_G1_LENGTH: usize = (TAU_POWERS_LENGTH << 1) - 1;
+pub struct TauParams {
+    pub g1_length: usize,
+    pub g2_length: usize,
+}
+
+impl TauParams {
+    pub fn new(tau_length: usize) -> TauParams {
+        TauParams {
+            g2_length: tau_length,
+            g1_length: (tau_length << 1) - 1,
+        }
+    }
+}
+
+/// TauPowers contains the actual values of the tau^i in G1 and G2 in affine
+/// form.
+pub struct TauPowers<E: Engine> {
+    pub tau_g1: Vec<E::G1Affine>,
+    pub tau_g2: Vec<E::G2Affine>,
+}
 
 /// read_vec reads up to size * point_length bytes from the reader and only
 /// verifies and returns "take" points - useful to advance the reader to a
@@ -53,37 +70,41 @@ fn read_vec<R: Read, C: PairingCurveAffine>(
     Ok(res_affine)
 }
 
-// read some bytes representing the hash - we are not verifying the hash chain
-// here so we don't need those bytes.
+/// read some bytes representing the hash - we are not verifying the hash chain
+/// here so we don't need those bytes.
 fn skip_hash<R: Read>(r: &mut R) {
     let mut tmp = [0; 64];
     r.read(&mut tmp)
         .expect("unable to read BLAKE2b hash of previous contribution");
 }
 
-pub struct TauPowers<E: Engine> {
-    pub tau_g1: Vec<E::G1Affine>,
-    pub tau_g2: Vec<E::G2Affine>,
-}
-
-fn read_powers_from_file<E: Engine>(fname: &str) -> Result<TauPowers<E>, DeserializationError> {
+fn read_powers_from_file<E: Engine>(
+    params: TauParams,
+    fname: &str,
+) -> Result<TauPowers<E>, DeserializationError> {
     let mut reader = OpenOptions::new()
         .read(true)
         .open(fname)
         .expect(&format!("unable open {} in this directory", fname));
-    read_powers(&mut reader)
+    read_powers(params, &mut reader)
 }
 
-fn read_powers_from_url<E: Engine>(url: &str) -> Result<TauPowers<E>, DeserializationError> {
+fn read_powers_from_url<E: Engine>(
+    params: TauParams,
+    url: &str,
+) -> Result<TauPowers<E>, DeserializationError> {
     let resp = isahc::get(url)?;
     let mut body = resp.into_body();
-    read_powers(&mut body)
+    read_powers(params, &mut body)
 }
 
-fn read_powers<E: Engine, R: Read>(reader: &mut R) -> Result<TauPowers<E>, DeserializationError> {
+fn read_powers<E: Engine, R: Read>(
+    params: TauParams,
+    reader: &mut R,
+) -> Result<TauPowers<E>, DeserializationError> {
     skip_hash(reader);
-    let g1p = read_vec::<_, E::G1Affine>(reader, TAU_POWERS_G1_LENGTH, 20)?;
-    let g2p = read_vec::<_, E::G2Affine>(reader, TAU_POWERS_LENGTH, 20)?;
+    let g1p = read_vec::<_, E::G1Affine>(reader, params.g1_length, 20)?;
+    let g2p = read_vec::<_, E::G2Affine>(reader, params.g2_length, 20)?;
     Ok(TauPowers {
         tau_g1: g1p,
         tau_g2: g2p,
@@ -91,13 +112,17 @@ fn read_powers<E: Engine, R: Read>(reader: &mut R) -> Result<TauPowers<E>, Deser
 }
 
 fn main() {
-    println!("Hello, world!");
-    //let zcash_file = "zcash_poweroftau.md";
-    //let zcash_acc = read_powers_from_file::<Bls12>(zcash_file);
+    println!("reading zcash taus!");
+    let zcash_params = TauParams::new(1 << 21);
+    let zcash_file = "zcash_poweroftau.md";
+    let zcash_acc = read_powers_from_file::<Bls12>(zcash_params, zcash_file);
     // last contribution for Filecoin's power of tau:
     // https://github.com/arielgabizon/perpetualpowersoftau/tree/master/0018_GolemFactory_response
+    println!("reading filecoin taus!");
+    let zcash_params = TauParams::new(1 << 21);
+    let fil_params = TauParams::new(1 << 27);
     let filecoin_url = "https://trusted-setup.s3.eu-central-1.amazonaws.com/challenge_18";
-    let filecoin_acc = read_powers_from_url::<Bls12>(filecoin_url);
+    let filecoin_acc = read_powers_from_url::<Bls12>(fil_params, filecoin_url);
 }
 /// Errors that might occur during deserialization.
 #[derive(Debug)]
